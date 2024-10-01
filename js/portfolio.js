b2d077d59402a75efcfcb5b07f2218f98d422c9e
Peko <erickms1816@gmail.com>
2024-10-01 20:20:36 -0300

    bro

--- js/portfolio.js
+++ js/portfolio.js
@@ -71,6 +71,7 @@ var modalImages = {
     ],
     2: [
         "../media/ci1.jpg",
+        "../media/ci2.JPG",
         "../media/ci3.jpg",
         "../media/ci4.jpg",
         "../media/ci5.png",
